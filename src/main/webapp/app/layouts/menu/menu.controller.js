0c9c2e747e8f86d2ebb96a56258bbfd362bf057b
gabriel@work <flying3615@163.com>
2016-04-21 15:02:11 +0800

    step1 menulist

---
+++ src/main/webapp/app/layouts/menu/menu.controller.js
@@ -0,0 +1,52 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('quayPsApp')
+        .controller('MenuController', MenuController);
+
+    MenuController.$inject = ['$scope', '$state', 'Menu'];
+
+    function MenuController ($scope, $state, Menu) {
+        var vm = this;
+        vm.menus = [];
+        vm.loadAll = function() {
+            Menu.query(function(result) {
+                vm.menus = result;
+            });
+        };
+
+        vm.loadAll();
+
+
+        $scope.menuList = [];
+
+        $http.get(mainService.baseUrl+'management/menu/getMenuByRoleForLogin').success(function(data){
+            $scope.menuList = changeToTree(data);
+        }).error(function(data){
+            $rootScope.authenticated = false;
+        })
+
+        //change data to tree data
+        var changeToTree = function(data){
+            var parentNodes = []
+            //pick up the parent
+            angular.forEach(data, function(data){
+                if(data.pId == '0'){
+                    parentNodes.push(data);
+                    data.child = []
+                }
+            });
+            //put all children to its parent
+            angular.forEach(parentNodes, function(parentNode){
+                angular.forEach(data, function(data){
+                    if(data.pId == parentNode.id){
+                        parentNode.child.push(data);
+                    }
+                });
+            });
+            return parentNodes;
+        }
+
+    }
+})();
---
+++ src/main/webapp/app/layouts/menu/menu.directive.js
@@ -0,0 +1,28 @@
+angular.module('mainApp').
+    directive('defMenu', function() {
+        return {
+            restrict:'A',
+            replace:true,
+            templateUrl:'app/layouts/menu/menuListPart.html',
+            controller:['$scope', function($scope){
+
+                //一开始将所有的全部设置为关闭的状态
+                for(var i = 0; i < $scope.menuList.length; i++){
+                    $scope.menuList[i].unfold = false;
+                }
+
+                //当点击某一个条目的时候，将条目设置为打开的状态，其余的设置为关闭的状态
+                $scope.itemClicked = function(item){
+                    for(var i = 0; i< $scope.menuList.length ;i++){
+                        if($scope.menuList[i].name == item.name){
+                            $scope.menuList[i].unfold = !$scope.menuList[i].unfold;
+                        }else{
+                            $scope.menuList[i].unfold = false;
+                        }
+                    }
+                }
+
+
+            }]
+        }
+    });
